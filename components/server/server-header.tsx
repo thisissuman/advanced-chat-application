eb7c1c35371c99b3ae2ced342fab26524ce6af8d
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-11-18 22:12:08 +0530

    commit

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -50,7 +50,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isAdmin && (
           <DropdownMenuItem
             onClick={() => onOpen("editServer", { server })}
-            className=" px-3 py-2 text-sm cursor-pointer"
+            className="px-3 py-2 text-sm cursor-pointer"
           >
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
@@ -59,7 +59,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isAdmin && (
           <DropdownMenuItem
             onClick={() => onOpen("members", { server })}
-            className=" px-3 py-2 text-sm cursor-pointer"
+            className="px-3 py-2 text-sm cursor-pointer"
           >
             Manage Members
             <UserIcon className="h-4 w-4 ml-auto" />
