3ed2f7f9827232a558dc6523b4731f6021f78256
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-11-22 23:38:39 +0530

    commit

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Hash, Menu } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
-import { UserAvatar } from "../user-avatar";
 import { SocketIndicator } from "../socket-indicator";
+import { UserAvatar } from "../user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -16,7 +16,7 @@ export const ChatHeader = ({
   name,
   type,
   imageUrl,
-}: ChatHeaderProps) => {
+}: ChatHeaderProps) => { 
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle serverId={serverId} />
