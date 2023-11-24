a856061bd4e37fbabb5cc766d49ba7defac10c1c
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-11-24 22:02:09 +0530

    message attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -10,6 +10,7 @@ import { Plus, Smile } from "lucide-react";
 import { Form, FormControl, FormItem, FormField } from "../ui/form";
 
 import { Input } from "../ui/input";
+import { useModal } from "@/hooks/use-modal-store";
 interface ChatInputProps {
   apiUrl: string;
   query: Record<string, any>;
@@ -21,6 +22,8 @@ const formSchema = z.object({
   content: z.string().min(1),
 });
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -35,7 +38,6 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
         query,
       });
       await axios.post(url, values);
-
     } catch (error) {
       console.log(error);
     }
@@ -52,7 +54,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {}}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px]
                     bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
