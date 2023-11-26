2a056d66fcb95abe2a4039d64f508e2cb52eee4e
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-11-26 18:32:55 +0530

    fix error

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -125,7 +125,7 @@ export const EditServerModal = () => {
               />
             </div>
             <DialogFooter className="bg-grey-100 px-6 py-4">
-              <Button disabled={isLoading} variant="priamry">
+              <Button disabled={isLoading} variant="primary">
                 Save
               </Button>
             </DialogFooter>
